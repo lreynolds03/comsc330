8fe7496c38c1607f8fc1e333c1c141c44b132d11
lreynolds03 <119824396+lreynolds03@users.noreply.github.com>
2024-04-09 19:50:12 -0400

    Update sec.py

--- sec.py
+++ sec.py
@@ -9,20 +9,22 @@ import re   # necessary to remove quotes from student info
 
 def sec(secFile):
 
-    with open (secFile, 'r') as file:
-        f = file.read()
+    with open (secFile, 'r') as file: # Opens file
+        f = file.read() # reads in entire file into f
 
     f = f.replace('"', '') # replacing quotation marks with blank space
     f = f.replace('\n', ',') # replaces each new line symbol with a comma
     f = f.split(',') # splits each element between commas
 
-    studentInfo = []
+    studentInfo = [] # initialize array 
     studentInfo.append(f[0]) # puts course name and credit hours as first element of array
 
-    for i in range(1, len(f), 4): # replaced split_f with split_nl
-        studentInfo.append(f[i:i+4]) # replaced split_f with split_nl
+    for i in range(1, len(f), 4): 
+        studentInfo.append(f[i:i+4]) # Appends 
 
     """
+    For loop to print out each student info 
+    
     for i in range(1, len(studentInfo)-1):
         print(studentInfo[i][3])
         print('\n')
@@ -34,6 +36,7 @@ def sec(secFile):
     return studentInfo 
 
 """
+Testing the class with section file
 def main():
 
     file = input('Enter the name of the file you want to run: ')
