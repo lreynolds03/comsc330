5e995716942deef5fcbdda7407749ca5ee4bd44f
lreynolds03 <119824396+lreynolds03@users.noreply.github.com>
2024-04-16 10:49:33 -0400

    Update sec_class.py

--- sec_class.py
+++ sec_class.py
@@ -17,10 +17,17 @@ def sec(secFile, outFile): # secFile is the file name of the section file
     f = f.replace('\n', ',') # replaces each new line symbol with a comma
     f = f.split(',') # splits each element between commas
 
-    studentInfo = [] # array to store student info
-    studentInfo.append(f[0]) # puts course name and credit hours as first element of array
+    ch = f[0][-3:] 
+    course = f[0][0:-6]
+
+    temp = []
+    temp.append(course)
+    temp.append(ch)
+    print(temp)
 
 
+    studentInfo = []
+    studentInfo.append(temp)
 
     for i in range(1, len(f)-1, 4): # i = 1-4, 5-8, 9-12, etc.
         studentInfo.append(f[i:i+4]) # Groups four items at a time starting at first students last name  
